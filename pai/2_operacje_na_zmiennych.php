9fb7b9ad865b1b4c6e488580e180d24392b66cd8
Kacper <90000906+Wozniak-Kacper@users.noreply.github.com>
2021-09-09 20:29:09 +0200

    Add files via upload

---
+++ pai/2_operacje_na_zmiennych.php
@@ -0,0 +1,30 @@
+<?php
+echo PHP_VERSION , "<br>"; //7.4.8
+echo 2**10, "<br>"; //potęgowanie
+
+$x=1;
+$y=10;
+echo $x<=>$y; //jesli lewa wieksza 1 , rowne 0 , prawwa -1
+
+//równe / identyczne 
+$x=1;
+$y=1.0;
+
+echo "<br>" .gettype($x); //integer
+echo "<br>" .gettype($y); //double
+
+if($x == $y) {
+    echo "równe";
+}else {
+    echo "różne";
+}
+
+if($x === $y) {
+    echo "identyczne";
+}else {
+    echo "różne";
+}
+
+
+
+?>
